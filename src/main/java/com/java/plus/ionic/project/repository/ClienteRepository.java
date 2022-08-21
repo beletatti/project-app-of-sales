edc80782992a610d1e3219aedbd8a044bcdb5dcc
Richard <richard.beletatti@gmail.com>
2022-08-21 01:34:28 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/java/plus/ionic/project/repository/ClienteRepository.java
+++ src/main/java/com/java/plus/ionic/project/repository/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.java.plus.ionic.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.java.plus.ionic.project.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
