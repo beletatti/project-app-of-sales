5a28a6eeef2b8e775682a6086f138d13e11e0b60
Richard <richard.beletatti@gmail.com>
2022-08-20 23:32:07 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/java/plus/ionic/project/service/CategoriaService.java
+++ src/main/java/com/java/plus/ionic/project/service/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.java.plus.ionic.project.domain.Categoria;
@@ -27,6 +30,11 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		List<Categoria> obj = categoriaRepository.findAll();
 		return obj;
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 
 	public Categoria insert(Categoria obj) {
