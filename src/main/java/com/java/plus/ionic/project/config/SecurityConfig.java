d79c09533dc98ae2c078e813c090352ad330bcd8
Richard <richard.beletatti@gmail.com>
2022-08-24 18:03:24 -0300

    Implementando autorizacao

--- src/main/java/com/java/plus/ionic/project/config/SecurityConfig.java
+++ src/main/java/com/java/plus/ionic/project/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.java.plus.ionic.project.security.JWTAuthenticationFilter;
+import com.java.plus.ionic.project.security.JWTAuthorizationFilter;
 import com.java.plus.ionic.project.security.JWTUtil;
 
 
@@ -50,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
