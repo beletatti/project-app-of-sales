b9beb80691fb1eef6fec71a9be8fd97e0f3d8429
Richard <richard.beletatti@gmail.com>
2022-08-24 17:50:45 -0300

    Criando mecanismo de autenticacao e gerando token com JWT

---
+++ src/main/java/com/java/plus/ionic/project/service/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.java.plus.ionic.project.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.java.plus.ionic.project.domain.Cliente;
+import com.java.plus.ionic.project.repository.ClienteRepository;
+import com.java.plus.ionic.project.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+}
