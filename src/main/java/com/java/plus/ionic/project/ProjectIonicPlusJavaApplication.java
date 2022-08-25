e72763f335c08b0b98c5d1aed05c54d88cd2c004
Richard <richard.beletatti@gmail.com>
2022-08-25 17:40:47 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/java/plus/ionic/project/ProjectIonicPlusJavaApplication.java
+++ src/main/java/com/java/plus/ionic/project/ProjectIonicPlusJavaApplication.java
@@ -1,17 +1,25 @@
 package com.java.plus.ionic.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.java.plus.ionic.project.service.S3Service;
+
 @SpringBootApplication
 public class ProjectIonicPlusJavaApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectIonicPlusJavaApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {		
+		s3Service.uploadFile("C:\\Users\\PC\\OneDrive\\Imagens\\Capturas de tela\\mc.jpg");
 	}	
 }
