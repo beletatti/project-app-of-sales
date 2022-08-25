6e8fac24230e3d9d8d2ed1f3d7be79830b839457
Richard <richard.beletatti@gmail.com>
2022-08-25 16:35:33 -0300

    Esqueci senha e Refresh token

--- src/main/java/com/java/plus/ionic/project/service/EmailService.java
+++ src/main/java/com/java/plus/ionic/project/service/EmailService.java
@@ -2,6 +2,7 @@ package com.java.plus.ionic.project.service;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.java.plus.ionic.project.domain.Cliente;
 import com.java.plus.ionic.project.domain.Pedido;
 
 
@@ -10,4 +11,6 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
